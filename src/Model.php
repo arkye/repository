c921e7f3552f9f4dcce8b9ce01789af39c832519
Gustavo Siqueira <gus@brid-it.com>
2022-02-22 16:27:48 -0300

    feat(model): getRepository method

--- src/Model.php
+++ src/Model.php
@@ -4,8 +4,21 @@ namespace Arkye\Repository;
 
 use Arkye\Repository\Concerns\EntityConvertible;
 use Arkye\Repository\Interfaces\IEntityConvertible as EntityConvertibleContract;
+use Arkye\Repository\Interfaces\IEntityRepository;
 
 class Model extends \Illuminate\Database\Eloquent\Model implements EntityConvertibleContract
 {
   use EntityConvertible;
+
+  public function getRepository(): IEntityRepository
+  {
+    $entityClass = str_replace('Infrastructure\\Models', 'Domain\\Entities', $this::class);
+
+    if (str_ends_with($entityClass, 'Model')) {
+      $entityClass = substr($entityClass, 0, strlen($entityClass) - 5);
+
+    }
+    return EntityManager::getRepository($entityClass);
+  }
+
 }
