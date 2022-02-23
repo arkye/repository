2b81dff8a421ef6050f53534b5986e48725c4f0e
Gustavo Siqueira <gus@brid-it.com>
2022-02-23 11:17:33 -0300

    feat: UrlRoutableEntity

---
+++ src/UrlRoutableEntity.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Arkye\Repository;
+
+use Illuminate\Contracts\Routing\UrlRoutable;
+use ReflectionException;
+
+class UrlRoutableEntity implements UrlRoutable
+{
+
+  public function getRouteKey()
+  {
+    return 'id';
+  }
+
+  public function getRouteKeyName()
+  {
+    return 'id';
+  }
+
+  /**
+   * Retrieve the model for a bound value.
+   *
+   * @param mixed $value
+   * @param string|null $field
+   * @return self|null
+   * @throws ReflectionException
+   */
+  public function resolveRouteBinding($value, $field = null): ?self
+  {
+    $model = EntityManager::getRepository($this::class)
+      ->newModel();
+
+    return $model
+      ->resolveRouteBindingQuery($model->newQuery(), $value, $field)
+      ->first()
+      ?->toEntity(null);
+  }
+
+  /**
+   * Retrieve the child model for a bound value.
+   *
+   * @param string $childType
+   * @param mixed $value
+   * @param string|null $field
+   * @return self|null
+   * @throws ReflectionException
+   */
+  public function resolveChildRouteBinding($childType, $value, $field): ?self
+  {
+    return EntityManager::getRepository($this::class)
+      ->newModel()
+      ->resolveChildRouteBindingQuery($childType, $value, $field)
+      ->first()
+      ?->toEntity(null);
+  }
+
+}
